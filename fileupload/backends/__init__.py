90783b58e1177c10b328e0def668193bfcec2c96
thethlaing10 <th.ucsy@gmail.com>
2020-05-05 20:11:01 +0630

    Django backend

--- fileupload/backends/__init__.py
+++ fileupload/backends/__init__.py
@@ -8,7 +8,7 @@ from django.conf import settings
 
 def get_backend():
     # Use S3 backend by default (current behaviour)
-    backend_setting = getattr(settings, "ORA2_FILEUPLOAD_BACKEND", "swift")
+    backend_setting = getattr(settings, "ORA2_FILEUPLOAD_BACKEND", "django")
     return django_storage.Backend()
     # if backend_setting == "s3":
     #     return s3.Backend()
